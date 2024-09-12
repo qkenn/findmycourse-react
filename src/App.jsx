902ef6c89a0c6366dca6995e547507b66c48b1e1
qkenn <github.com.sampling990@passinbox.com>
2024-09-12 10:37:23 +0530

    create basic form

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
 export function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <h1>Check eligibility for a programme</h1>
+        <form>
+          <label>
+            subject stream
+            <select name="stream">
+              <option value="arts">Arts</option>
+              <option value="commerce">Commerce</option>
+              <option value="biology">Biologycal Science</option>
+              <option value="physical">Physical Science</option>
+              <option value="other">Other</option>
+            </select>
+          </label>
+
+          <label>
+            <input type="text" placeholder="course" />
+          </label>
+
+          <button>Search</button>
+        </form>
+      </header>
+    </>
+  );
 }
