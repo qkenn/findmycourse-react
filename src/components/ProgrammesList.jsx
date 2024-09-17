b315436dc3dfdf759f7db3f50b39d15964f0d576
qkenn <github.com.sampling990@passinbox.com>
2024-09-17 17:01:13 +0530

    update programme card styling

--- src/components/ProgrammesList.jsx
+++ src/components/ProgrammesList.jsx
@@ -8,14 +8,30 @@ export function ProgrammesList({ courses, searchError, isLoading }) {
           <li key={p.id} className="rounded-sm bg-white p-7">
             <div>
               <h3 className="text-lg">
-                <Link to={`explore/programmes`}>
+                <Link
+                  to={`explore/programmes/${p.id}`}
+                  className="hover:underline"
+                >
                   <strong>{p.name}</strong>
                 </Link>
               </h3>
-              <Link to={`explore/universities`}>{p.university?.name}</Link>
+              <Link
+                to={`explore/universities/${p.universityId}`}
+                className="hover:underline"
+              >
+                {p.university?.name}
+              </Link>
 
               <div className="mt-5">
-                <div>Course: {p.course?.name}</div>
+                <div>
+                  Course:{' '}
+                  <Link
+                    to={`explore/courses/${p.courseId}`}
+                    className="hover:underline"
+                  >
+                    {p.course?.name}
+                  </Link>
+                </div>
                 <div>Duration: {p.duration}</div>
                 <div>Medium: {p.medium}</div>
               </div>
