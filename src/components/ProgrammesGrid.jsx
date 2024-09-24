9afd3beafd510af88106454c9dab294d182e3832
qkenn <github.com.sampling990@passinbox.com>
2024-09-24 17:49:29 +0530

    refact: create a parent component for filters and programmes listing in homepage

---
+++ src/components/ProgrammesGrid.jsx
@@ -0,0 +1,13 @@
+import { Filters } from './Filters';
+import { HomeProgrammes } from './HomeProgrammes';
+
+export default function ProgrammesGrid() {
+  return (
+    <main className="my-10 px-5">
+      <div className="mx-auto grid max-w-[62.5rem] grid-cols-12 gap-10">
+        <Filters />
+        <HomeProgrammes />
+      </div>
+    </main>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,9 @@
 import { createContext, useEffect, useReducer } from 'react';
 import { Hero } from '../components/Hero';
-import { Filters } from '../components/Filters';
-import { HomeProgrammes } from '../components/HomeProgrammes';
 import { programmesReducer } from '../reducers/programmesReducer';
 import { FiltersReducer } from '../reducers/filtersReducer';
 import { searchProgrammes } from '../utils/searchProgrammes';
+import ProgrammesGrid from '../components/ProgrammesGrid';
 
 export const ProgrammeContext = createContext();
 
@@ -32,12 +31,7 @@ export function HomePage() {
       >
         <Hero />
 
-        <main className="my-10 px-5">
-          <div className="mx-auto grid max-w-[62.5rem] grid-cols-12 gap-10">
-            <Filters />
-            <HomeProgrammes />
-          </div>
-        </main>
+        {programmes.results && <ProgrammesGrid />}
       </ProgrammeContext.Provider>
     </>
   );
