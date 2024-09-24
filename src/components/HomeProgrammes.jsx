4f344a3d417e8454d200dd127529081b014e7c3f
qkenn <github.com.sampling990@passinbox.com>
2024-09-24 22:25:49 +0530

    refact: create seperate component for pagination links

--- src/components/HomeProgrammes.jsx
+++ src/components/HomeProgrammes.jsx
@@ -2,16 +2,14 @@ import { useContext } from 'react';
 import { ProgrammeContext } from '../pages/HomePage';
 import { HomeProgrammeCard } from './HomeProgrammeCard';
 import { Loading } from './Loading';
+import PaginationLink from './PaginationLink';
 
 export function HomeProgrammes() {
-  const { programmes, searchProgrammes, programmesDispatch } =
-    useContext(ProgrammeContext);
+  const { programmes } = useContext(ProgrammeContext);
   const pagesCount =
     (programmes.count || 0) <= (programmes.pageSize || 8)
       ? 1
       : Math.ceil((programmes.count || 0) / (programmes.pageSize || 8));
-
-  console.log(programmes.pageSize, programmes.count, pagesCount);
 
   return (
     <ul className="col-span-8 flex flex-col gap-10">
@@ -26,21 +24,7 @@ export function HomeProgrammes() {
         <div className="flex justify-center gap-3">
           {[...Array(pagesCount)].map((_, index) => (
             // pagination links
-            <button
-              disabled={index + 1 === programmes.page}
-              className={`${index + 1 === programmes.page ? 'rounded-sm bg-neutral-950 px-2 py-0.5 text-white' : 'hover:underline'}`}
-              key={index}
-              onClick={() => {
-                const pageNumber = index + 1;
-                searchProgrammes(
-                  programmesDispatch,
-                  programmes.query,
-                  pageNumber
-                );
-              }}
-            >
-              {index + 1}
-            </button>
+            <PaginationLink key={index} index={index} />
           ))}
         </div>
       )}
