95fabf67223b3ddda0ea9e3d4ee53ec784ac6178
qkenn <github.com.sampling990@passinbox.com>
2024-09-24 15:50:07 +0530

    add close button to filter overlay

--- src/components/FilterOverlay.jsx
+++ src/components/FilterOverlay.jsx
@@ -13,10 +13,17 @@ export default function FilterOverlay({
 
   return (
     <div className="fixed inset-0 z-10 flex items-center justify-center bg-neutral-700 bg-opacity-50 p-10">
-      <div className="max-h-[85dvh] max-w-[40rem] overflow-auto bg-white px-16 py-14">
+      <div className="max-h-[85dvh] max-w-[40rem] overflow-auto bg-white p-12">
         <div className="flex justify-between">
           <h3 className="text-2xl font-semibold">{title}</h3>
-          <button onClick={closeOverlay}>closeOverlay</button>
+          <button
+            onClick={closeOverlay}
+            className="flex items-center justify-center rounded-sm bg-neutral-950 p-1"
+          >
+            <svg width="25" height="25" style={{ fill: '#fff' }}>
+              <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
+            </svg>
+          </button>
         </div>
         <ul className="mt-5 flex flex-col divide-y">
           <li
