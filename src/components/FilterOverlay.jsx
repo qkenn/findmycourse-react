5480d993fdba83ff81171a0d62bb7f4d61a47364
qkenn <github.com.sampling990@passinbox.com>
2024-09-24 16:34:14 +0530

    refact: create overlay close btn

--- src/components/FilterOverlay.jsx
+++ src/components/FilterOverlay.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import DefaultFilterOption from './DefaultFilterOption';
 import FilterOption from './FilterOption';
+import OverlayCloseBtn from './OverlayCloseBtn';
 
 export default function FilterOverlay({
   options,
@@ -15,15 +16,9 @@ export default function FilterOverlay({
       <div className="max-h-[85dvh] max-w-[40rem] overflow-auto bg-white p-12">
         <div className="flex justify-between">
           <h3 className="text-2xl font-semibold">{title}</h3>
-          <button
-            onClick={closeOverlay}
-            className="flex items-center justify-center rounded-sm bg-neutral-950 p-1"
-          >
-            <svg width="25" height="25" style={{ fill: '#fff' }}>
-              <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
-            </svg>
-          </button>
+          <OverlayCloseBtn closeOverlay={closeOverlay} />
         </div>
+
         <ul className="mt-5 flex flex-col divide-y">
           <DefaultFilterOption
             handleReset={() => setResetSelected((r) => !r)}
---
+++ src/components/OverlayCloseBtn.jsx
@@ -0,0 +1,12 @@
+export default function OverlayCloseBtn({ closeOverlay }) {
+  return (
+    <button
+      onClick={closeOverlay}
+      className="flex items-center justify-center rounded-sm bg-neutral-950 p-1"
+    >
+      <svg width="25" height="25" style={{ fill: '#fff' }}>
+        <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
+      </svg>
+    </button>
+  );
+}
