7896a4393c5e5c760533aa8fadd23da23f58eb6f
qkenn <github.com.sampling990@passinbox.com>
2024-09-25 09:55:45 +0530

    feat: display pagination info after search

--- src/components/ProgrammesGrid.jsx
+++ src/components/ProgrammesGrid.jsx
@@ -1,12 +1,21 @@
+import { useContext } from 'react';
 import { FiltersContainer } from './FiltersContainer';
 import { HomeProgrammes } from './HomeProgrammes';
+import { ProgrammeContext } from '../pages/HomePage';
+import PaginationInfo from './PaginationInfo';
 
 export default function ProgrammesGrid() {
+  const { programmes } = useContext(ProgrammeContext);
+
   return (
     <main className="my-10 px-5">
-      <section className="mx-auto grid max-w-[62.5rem] grid-cols-12 gap-10">
-        <FiltersContainer />
-        <HomeProgrammes />
+      <section className="mx-auto max-w-[62.5rem]">
+        {programmes.results && <PaginationInfo />}
+
+        <div className="mt-3 grid grid-cols-12 gap-10">
+          <FiltersContainer />
+          <HomeProgrammes />
+        </div>
       </section>
     </main>
   );
