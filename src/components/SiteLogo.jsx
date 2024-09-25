2fed7850e434808a93308586a0bb325307b01f1d
qkenn <github.com.sampling990@passinbox.com>
2024-09-26 03:55:41 +0530

    update site logo

--- src/components/SiteLogo.jsx
+++ src/components/SiteLogo.jsx
@@ -3,10 +3,7 @@ import { Link } from 'react-router-dom';
 export default function SiteLogo() {
   return (
     <Link to="/">
-      <h3 className="text-white">
-        <span className="font-semibold">U</span>NI
-        <span className="font-semibold">C</span>OURSE
-      </h3>
+      <h2 className="text-3xl">🏫</h2>
     </Link>
   );
 }
