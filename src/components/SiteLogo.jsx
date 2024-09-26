e5f5cdfc1a933a7f146dcb7cf1666af6f11b94c2
qkenn <github.com.sampling990@passinbox.com>
2024-09-26 13:11:30 +0530

    update site logo

--- src/components/SiteLogo.jsx
+++ src/components/SiteLogo.jsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 export default function SiteLogo() {
   return (
     <Link to="/">
-      <h2 className="text-3xl">🏫</h2>
+      <div className="h-[50px] w-[50px]">
+        <img src="/site-logo.svg" alt="Site Logo" />
+      </div>
     </Link>
   );
 }
