1471eedf3cdb5188ed9c096bdbb5bc293e6fed35
qkenn <github.com.sampling990@passinbox.com>
2024-10-07 23:15:41 +0530

    update site logo

--- src/components/SiteLogo.jsx
+++ src/components/SiteLogo.jsx
@@ -3,8 +3,12 @@ import { Link } from 'react-router-dom';
 export default function SiteLogo() {
   return (
     <Link to="/">
-      <div className="h-[50px] w-[50px]">
-        <img src="/site-logo.svg" alt="Site Logo" />
+      <div className="flex items-center justify-center gap-2">
+        <div className="h-[30px] w-[30px]">
+          <img src="/site-logo.svg" alt="Site Logo" />
+        </div>
+
+        <h4 className="pt-1 text-white">unicourse</h4>
       </div>
     </Link>
   );
