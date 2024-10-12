c32a4b2ed14ed852e51a6eadf98f0ce6a0c5a857
qkenn <github.com.sampling990@passinbox.com>
2024-10-12 13:08:00 +0530

    fix(deploy): netlify build error
    temprarly remove explore routes from react router dom,
    reason: api isn't available for data fetching.

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { MainLayout } from './ui/layouts/MainLayout';
 import { AppErrorPage, NotFoundPage } from './ui/errors/PageErrors';
-import EntityDetailsPage from './ui/pages/EntityDetailsPage';
-import { EntityPage } from './ui/pages/EntityPage';
+// import EntityDetailsPage from './ui/pages/EntityDetailsPage';
+// import { EntityPage } from './ui/pages/EntityPage';
 import HomePage from './ui/pages/HomePage';
-import { ExplorePage } from './ui/pages/ExplorePage';
+// import { ExplorePage } from './ui/pages/ExplorePage';
 
 export const router = createBrowserRouter([
   {
@@ -16,103 +16,103 @@ export const router = createBrowserRouter([
         element: <NotFoundPage />,
       },
       { path: '/', element: <HomePage /> },
-      {
-        path: '/explore',
-        children: [
-          { index: true, element: <ExplorePage /> },
-          {
-            path: 'subjects',
-            children: [
-              {
-                index: true,
-                element: <EntityPage />,
-                loader: ({ request: { signal } }) => {
-                  return fetch('http://localhost:8080/api/subjects', signal);
-                },
-              },
-              {
-                path: ':id',
-                element: <EntityDetailsPage />,
-                loader: ({ params: { id }, request: { signal } }) => {
-                  return fetch(
-                    `http://localhost:8080/api/subjects/${id}`,
-                    signal
-                  );
-                },
-              },
-            ],
-          },
-          {
-            path: 'courses',
-            children: [
-              {
-                index: true,
-                element: <EntityPage />,
-                loader: ({ request: { signal } }) => {
-                  return fetch('http://localhost:8080/api/courses', signal);
-                },
-              },
-              {
-                path: ':id',
-                element: <EntityDetailsPage />,
-                loader: ({ params: { id }, request: { signal } }) => {
-                  return fetch(
-                    `http://localhost:8080/api/courses/${id}`,
-                    signal
-                  );
-                },
-              },
-            ],
-          },
-          {
-            path: 'programmes',
-            children: [
-              {
-                index: true,
-                element: <EntityPage />,
-                loader: ({ request: { signal } }) => {
-                  return fetch('http://localhost:8080/api/programmes', signal);
-                },
-              },
-              {
-                path: ':id',
-                element: <EntityDetailsPage />,
-                loader: ({ params: { id }, request: { signal } }) => {
-                  return fetch(
-                    `http://localhost:8080/api/programmes/${id}`,
-                    signal
-                  );
-                },
-              },
-            ],
-          },
-          {
-            path: 'universities',
-            children: [
-              {
-                index: true,
-                element: <EntityPage />,
-                loader: ({ request: { signal } }) => {
-                  return fetch(
-                    'http://localhost:8080/api/universities',
-                    signal
-                  );
-                },
-              },
-              {
-                path: ':id',
-                element: <EntityDetailsPage />,
-                loader: ({ params: { id }, request: { signal } }) => {
-                  return fetch(
-                    `http://localhost:8080/api/universities/${id}`,
-                    signal
-                  );
-                },
-              },
-            ],
-          },
-        ],
-      },
+      // {
+      //   path: '/explore',
+      //   children: [
+      //     { index: true, element: <ExplorePage /> },
+      //     {
+      //       path: 'subjects',
+      //       children: [
+      //         {
+      //           index: true,
+      //           element: <EntityPage />,
+      //           loader: ({ request: { signal } }) => {
+      //             return fetch('http://localhost:8080/api/subjects', signal);
+      //           },
+      //         },
+      //         {
+      //           path: ':id',
+      //           element: <EntityDetailsPage />,
+      //           loader: ({ params: { id }, request: { signal } }) => {
+      //             return fetch(
+      //               `http://localhost:8080/api/subjects/${id}`,
+      //               signal
+      //             );
+      //           },
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       path: 'courses',
+      //       children: [
+      //         {
+      //           index: true,
+      //           element: <EntityPage />,
+      //           loader: ({ request: { signal } }) => {
+      //             return fetch('http://localhost:8080/api/courses', signal);
+      //           },
+      //         },
+      //         {
+      //           path: ':id',
+      //           element: <EntityDetailsPage />,
+      //           loader: ({ params: { id }, request: { signal } }) => {
+      //             return fetch(
+      //               `http://localhost:8080/api/courses/${id}`,
+      //               signal
+      //             );
+      //           },
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       path: 'programmes',
+      //       children: [
+      //         {
+      //           index: true,
+      //           element: <EntityPage />,
+      //           loader: ({ request: { signal } }) => {
+      //             return fetch('http://localhost:8080/api/programmes', signal);
+      //           },
+      //         },
+      //         {
+      //           path: ':id',
+      //           element: <EntityDetailsPage />,
+      //           loader: ({ params: { id }, request: { signal } }) => {
+      //             return fetch(
+      //               `http://localhost:8080/api/programmes/${id}`,
+      //               signal
+      //             );
+      //           },
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       path: 'universities',
+      //       children: [
+      //         {
+      //           index: true,
+      //           element: <EntityPage />,
+      //           loader: ({ request: { signal } }) => {
+      //             return fetch(
+      //               'http://localhost:8080/api/universities',
+      //               signal
+      //             );
+      //           },
+      //         },
+      //         {
+      //           path: ':id',
+      //           element: <EntityDetailsPage />,
+      //           loader: ({ params: { id }, request: { signal } }) => {
+      //             return fetch(
+      //               `http://localhost:8080/api/universities/${id}`,
+      //               signal
+      //             );
+      //           },
+      //         },
+      //       ],
+      //     },
+      //   ],
+      // },
     ],
   },
 ]);
